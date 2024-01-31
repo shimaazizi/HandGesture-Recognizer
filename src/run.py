8488b45e14b32f4852e5d8856c84cafebbb87612
shima98 <shima.azizimajd98@gmail.com>
2024-01-31 20:09:52 +0330

    add simple cnn

--- src/run.py
+++ src/run.py
@@ -7,35 +7,9 @@ import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
-
-from sklearn.preprocessing import LabelEncoder
-from tensorflow.keras.utils import to_categorical
-
 from tensorflow.keras import layers, models
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from keras.utils import to_categorical
-
-from tensorflow.keras.layers import (
-    Layer,
-    GlobalAveragePooling2D,
-    Conv2D,
-    MaxPool2D,
-    Dense,
-    Flatten,
-    InputLayer,
-    BatchNormalization,
-    Input,
-    Dropout,
-    RandomFlip,
-    RandomRotation,
-    RandomContrast,
-    RandomBrightness,
-    Resizing,
-    Rescaling
-)
-from tensorflow.keras.losses import BinaryCrossentropy, CategoricalCrossentropy, SparseCategoricalCrossentropy
-from tensorflow.keras.optimizers import Adam
-
 
 
 
@@ -90,7 +64,8 @@ def display(train_images, train_labels, num_images_to_display):
 
 # Prepare the dataset
 train_images, train_labels = prepare_dataset(output_directory)
- 
+#print(train_images.shape)
+
 # Display the images
 display(train_images, train_labels, num_images_to_display=4)
 
@@ -98,6 +73,7 @@ display(train_images, train_labels, num_images_to_display=4)
 
 # Resize anb Normalization the images
 train_images = np.array([np.array(Image.fromarray(image_array).resize((200, 200))).astype('float32') / 255.0 for image_array in train_images])
+#print(train_images.shape)
 
 # Convert the labels to a NumPy array
 train_labels = np.array(train_labels)
@@ -113,19 +89,27 @@ test_images, val_images, test_labels, val_labels = train_test_split(
 )
 
 
+
+
 num_classes = len(set(train_labels))
 print(f"Number of classes: {num_classes}")
 
-# One hot encodeing for train_labels and val_labels
+# one hot encodeing for labels
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.utils import to_categorical
+
+
 label_encoder = LabelEncoder()
 train_labels_encoded = label_encoder.fit_transform(train_labels)
 train_labels_one_hot = to_categorical(train_labels_encoded, num_classes=4)
+#print("train_labels_one_hot:")
+#print(train_labels_one_hot.shape)
 
 val_labels_encoded = label_encoder.transform(val_labels)
 val_labels_one_hot = to_categorical(val_labels_encoded, num_classes=4)
+#print("val_labels_one_hot:")
+#print(val_labels_one_hot.shape)
 
-
-# Apply data augmentation 
 train_datagen_augmented = ImageDataGenerator(
     rotation_range=10,
     width_shift_range=0.1,
@@ -137,44 +121,93 @@ train_datagen_augmented = ImageDataGenerator(
 train_data_augmented = train_datagen_augmented.flow(
     train_images, train_labels_one_hot, batch_size=64
 )
-val_datagen_augmented = ImageDataGenerator()   
+val_datagen_augmented = ImageDataGenerator()  # You can specify augmentation parameters if needed
 val_data_augmented = val_datagen_augmented.flow(
     val_images, val_labels_one_hot, batch_size=32
 )
 
 
-# Model
-# transfer learning
-weights_path = "/home/shima/efficientnetv2-b0_notop.h5"
-backbone = tf.keras.applications.EfficientNetV2B0(
-    include_top=False,
-    weights=weights_path,
-    input_shape=(200, 200, 3),
-    include_preprocessing=True
-)
 
-backbone.trainable = False
+#  create model 
+# Set random seed
+tf.random.set_seed(42)
 
-efficient_model = tf.keras.Sequential([
-    Input(shape=(200, 200, 3)),
-    backbone,
-    GlobalAveragePooling2D(),
-    Dense(1024, activation='relu'),
-    BatchNormalization(),
-    Dense(128, activation='relu'),
-    Dense(4, activation='softmax')
-])
+model = models.Sequential()
+model.add(layers.Conv2D(10, (3, 3), activation='relu', input_shape=(200, 200, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(10, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(10, activation='relu'))
+model.add(layers.Dense(4, activation='softmax'))
 
-efficient_model.compile(
-    optimizer = Adam(learning_rate=0.001),
-    loss = CategoricalCrossentropy(),
-    metrics=["accuracy"]
-)
 
-# Model Training 
-efficient_history = efficient_model.fit(
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
+model.compile(optimizer=optimizer,
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+history = model.fit(
     train_data_augmented,
-    validation_data = val_data_augmented,
-    epochs = 10,
-    verbose = 1
+    steps_per_epoch=len(train_images) // 64,  
+    epochs=5,
+    validation_data=val_data_augmented
 )
+
+# plot the accuracy and loss
+
+def plot_and_save_curves(history, filename="curves.png"):
+    """
+    Plots and saves separate loss and accuracy curves for training and validation metrics.
+    """
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    accuracy = history.history['accuracy']
+    val_accuracy = history.history['val_accuracy']
+
+    epochs = range(len(history.history['loss']))
+
+    # Plot loss and accuracy on the same figure
+    
+    # Plot loss
+    plt.plot(epochs, loss, label='training_loss')
+    plt.plot(epochs, val_loss, label='val_loss')
+    plt.title('Loss')
+    plt.xlabel('Epochs')
+    plt.legend()
+    
+    plt.savefig(filename.replace(".png", "_loss.png"))
+    plt.close()
+
+    # Plot accuracy
+    plt.figure()
+    plt.plot(epochs, accuracy, label='training_accuracy')
+    plt.plot(epochs, val_accuracy, label='val_accuracy')
+    plt.title('Accuracy')
+    plt.xlabel('Epochs')
+    plt.legend();
+    
+    # Save the figure
+    plt.savefig(filename.replace(".png", "_accuracy.png"))
+    plt.close()
+
+plot_and_save_curves(history, filename="curves.png")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
